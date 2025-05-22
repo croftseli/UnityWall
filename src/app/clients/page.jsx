c1f4032584b2bfb393e84f12976746e648dd81e7
MichaelSanei <michaelnabil.sanei@gmail.com>
2025-05-22 22:13:54 +0100

    changed mochinut link to vercel

--- src/app/clients/page.jsx
+++ src/app/clients/page.jsx
@@ -13,7 +13,7 @@ const clients = [
       "A vibrant website for Mochinut Spring Hill, a specialty dessert shop known for their unique mochi donuts and innovative flavor combinations. The project aims to create an engaging online presence that showcases their colorful, creative menu offerings in a fun and interactive manner.",
     logo: "/images/mochiNutLogo.jpg",
     technologies: ["Next.js", "Tailwind CSS", "Framer Motion"],
-    link: "https://mochinutspringhill.com",
+    link: "https://mochinut-sandy.vercel.app",
   },
   {
     id: 2,
