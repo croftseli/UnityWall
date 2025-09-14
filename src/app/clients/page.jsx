c66c38fde917caf9607f084aeb7620036c5e8467
Elijah Crofts <elicrofts@gmail.com>
2025-09-14 19:58:31 -0400

    Copper to Gold Client add

--- src/app/clients/page.jsx
+++ src/app/clients/page.jsx
@@ -8,6 +8,15 @@ import CTA from "@/components/layout/CTA";
 const clients = [
   {
     id: 1,
+    title: "Copper to Gold",
+    description:
+      "Copper to Gold is a non-profit organization dedicated to achieving race amity through personal trasformation. Their website will be redesigned and will aid in Copper to Gold's rapid expansion. Custom API's will be built to manage events, donations, and user accounts. Coming soon!",
+    logo: "/images/copperToGold3.png",
+    technologies: ["Next.js", "Tailwind CSS", "Framer Motion"],
+    bgGradient: "from-gray-600 via-gray-700 to-gray-700",
+  },
+  {
+    id: 2,
     title: "Collaborative Developments LLC",
     description:
       "The website for Collaborative Developments LLC will serve as an online presence to communicate the company's mission, activities, and principles. The website will aid in providing a face for the initiative, helping to attain their goal of increasing investment capital from $1M to $2M.",
@@ -17,7 +26,7 @@ const clients = [
     bgGradient: "from-gray-600 via-gray-700 to-gray-700",
   },
   {
-    id: 2,
+    id: 3,
     title: "Safayra Saffron",
     description:
       "Based in Luxembourg, Safayra is a premium importer that offers high-quality saffron sourced from the best farms in Iran. This work-in-progress will showcase the brand's commitment to quality and provides an elegant experience for customers.",
@@ -27,7 +36,7 @@ const clients = [
     bgGradient: "from-gray-600 via-gray-700 to-gray-700",
   },
   {
-    id: 3,
+    id: 4,
     title: "Cossa Embroidery",
     description:
       "Embroidery service based in Franklin, TN. This project is the online face of Cossa, showcasing their portfolio, and providing a platform for customers to learn more about their offerings.",
@@ -37,7 +46,7 @@ const clients = [
     bgGradient: "from-gray-600 via-gray-700 to-gray-700", // Brightened from gray-700/800/800
   },
   {
-    id: 4,
+    id: 5,
     title: "Permanent Makeup by Lauren",
     description:
       "A modern website for Permanent Makeup by Lauren, a beauty service specializing in permanent makeup solutions. The project will focus on creating an elegant and user-friendly experience to showcase services and attract clients.",
@@ -45,9 +54,8 @@ const clients = [
     technologies: ["Coming Soon"],
     bgGradient: "from-gray-600 via-gray-700 to-gray-700", // Brightened from gray-700/800/800
   },
-
   {
-    id: 5,
+    id: 6,
     title: "Centro Studi Baha'i, Hotel la Panoramica - Acuto, Italy",
     description:
       "A modern website for Centro Studi Baha'i - Hotel Panoramica, a hotel and conference center located in Aculto, Italy. The project aimed to create a user-friendly experience that highlights the hotel's amenities and scenic surroundings.",
